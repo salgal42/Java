3feeb0cf94bb1ad52f928c751a93f4df3bb010f8
Salatiel Galvão Zambelino <84423199+salgal42@users.noreply.github.com>
2023-05-14 20:17:35 -0300

    Adicionei arquivos via upload

---
+++ Produto Github/src/br/com/salgal42/git/ProdutoGit.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package br.com.salgal42.git;
+
+/**
+ * @author Salatiel Galvão Zambelino
+ * Programa produto entre 11 e 78
+ */
+public class ProdutoGit {
+    	
+	public static void main(String[] args) {
+		
+		int n1 = 11;
+		int n2 = 78;
+		int produto;
+		
+		produto = n1 * n2;
+		System.out.println(produto);
+		
+		
+		
+		
+
+	}
+
+}
